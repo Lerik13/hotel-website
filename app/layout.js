edae74391750c5187ea30dcee7173e8d85eb4d61
Valeria <lerik13@gmail.com>
2025-02-01 11:58:27 -0700

    update RootLayout, improove Navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-import Logo from '@/app/_components/Logo'
-import Navigation from '@/app/_components/Navigation'
+import Header from './_components/Header'
 
 import { Josefin_Sans } from 'next/font/google'
 
@@ -24,14 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-900 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-900 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by the Mountain Villa</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   )
